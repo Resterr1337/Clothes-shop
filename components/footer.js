3bbf3bc6cd4bc23b71c60c6a39ecd90a8fe86ef5
Resterr1337 <Franklion1chance@gmail.com>
2023-03-23 13:13:12 +0500

    About-brand added

---
+++ components/footer.js
@@ -0,0 +1,42 @@
+document.write(`
+<footer>
+    <div class="wrapper">
+        <div class="part-1">
+            <a href="" class="logo">
+                <img src="./assets/images/logo.svg" alt="" srcset="">
+                <span>WOMAZING</span>
+            </a>
+            <div class="text-wrap">
+                <span>© Все права защищены</span>
+                <a href="">Политика конфиденциальности</a>
+                <a href="">Публичная оферта</a>
+            </div>
+        </div>
+        <div class="part-2">
+            <nav>
+                <ul>
+                    <li><a class="active" href="index.html">Главная</a></li>
+                    <li><a class="deactive" href="shop.html">Магазин</a></li>
+                    <li><a class="deactive" href="about-brand.html">О бренде</a></li>
+                    <li><a class="deactive" href="contacts.html">Контакты</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="part-3">
+            <ul>
+                <li>+7 (495) 823-54-12</li>
+                <li>hello@womazing.com</li>
+                <li><div class="links">
+                    <a href=""><img src="./assets/images/instagram.svg" alt=""></a>
+                    <a href=""><img src="./assets/images/facebook.svg" alt=""></a>
+                    <a href=""><img src="./assets/images/twitter.svg" alt=""></a>
+                </div></li>
+                <li>
+                    <div class="supported-type-of-purchase">
+                        <img src="./assets/images/visa-mastercard.png" alt="">
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>`) 
---
+++ scripts/about-brand.js
@@ -0,0 +1,4 @@
+function goToMarketPlaceFunc() {
+    document.querySelector("#onescreen-btn").classList.add("btn-wrapper-active")
+    setTimeout(() => {window.location = "shop.html"}, 0.5 * 1000)
+}
